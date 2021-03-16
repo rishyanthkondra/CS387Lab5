32b95e14393fbf304c984856d28e842c7dbf9284
AbhinavGoudBingi <bingiabhinavgoud@gmail.com>
2021-03-16 15:19:20 +0530

    fixed bug in buy button

--- controllers/orders.js
+++ controllers/orders.js
@@ -8,9 +8,13 @@ exports.order = async (req,res,next) => {
     const cartvalue = await orderitem.get_cartvalue().catch(err => console.log(err));
     if(credits.rows[0].credit >= cartvalue.rows[0].cartvalue){
         await orderitem.order(cartvalue.rows[0].cartvalue).catch(err => console.log(err));
+        res.redirect('/orders');
+    }
+    else{
+        res.redirect('/cart');
     }
 
-    res.redirect('/orders');
+    
 };
 
 exports.get_orders = async (req,res,next) => {
--- models/orders.js
+++ models/orders.js
@@ -11,18 +11,15 @@ module.exports = class Orders{
     async order(cartvalue){
         //see if order already placed for the object
         //insert if not present 
-        /*await pool.query('INSERT INTO orders(user_id,item_id,quantity) 
-                        SELECT cart.user_id,cart.item_id,cart.quantity FROM cart WHERE ')*/
+        //await pool.query('INSERT INTO orders(user_id,item_id,quantity) SELECT cart.user_id,cart.item_id,cart.quantity FROM cart WHERE ')
         //else update
-        /*await pool.query('
-            INSERT into orders(user_id,item_id,quantity) 
-            select cart.user_id,cart.item_id,
-                coalesce((select 
-                            quantity from orders
-                            where user_id = 1 and item_id = cart.item_id
-                        ),0)+cart.quantity 
-            from cart where user_id = 1;');*/
-        await pool.query('DELETE from cart where user_id = 1;')
+       // await pool.query('UPDATE orders(user_id,item_id,quantity) select cart.user_id,cart.item_id,coalesce((select quantity from orders where user_id = 1 and item_id = cart.item_id),0)+cart.quantity from orders left outer join cart on  where user_id = 1;')
+       const cart_items = await pool.query('SELECT item_id from cart')
+       for(var i=0;i<cart_items.rows.length;i++){
+           const query_s = "do $$ DECLARE item int = "+cart_items.rows[i].item_id+"; begin IF EXISTS (SELECT quantity from orders where item_id = item and user_id = 1) THEN UPDATE orders set quantity = quantity+(select quantity from cart where item_id = item and user_id = 1) where item_id = item and user_id = 1; ELSE INSERT INTO orders SELECT * from cart where item_id = item and user_id = 1; END IF; end $$";
+           await pool.query({text: query_s});
+       }
+       await pool.query('DELETE from cart where user_id = 1;')
         return pool.query('UPDATE users set credit = credit-$1 where user_id = 1;',[cartvalue]);
     }
     get_cartvalue(){
